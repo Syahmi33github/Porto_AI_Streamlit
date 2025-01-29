c4d88e75efc796c9ee8c14454f5f658771760736
Syahmi Sajid <syahmisajid@student.untan.ac.id>
2025-01-29 13:34:59 +0700

    add new project talk to pdf

--- Chat_with_Your_PDF/chatbot.py
+++ Chat_with_Your_PDF/chatbot.py
@@ -41,6 +41,7 @@ st.markdown(
 )
 
 import streamlit as st
+import os
 
 from dotenv import load_dotenv # langfuse or opik
 from langchain_ollama import ChatOllama
@@ -56,18 +57,58 @@ from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_community.chat_message_histories import SQLChatMessageHistory
 
 from langchain_core.output_parsers import StrOutputParser
+from langchain_community.document_loaders import PyMuPDFLoader
 
 load_dotenv('./../.env')
 
 # Tampilkan teks dengan warna putih menggunakan kelas CSS
-st.markdown('<div class="title">What can I help with?</div>', unsafe_allow_html=True)
+st.markdown('<div class="title">Chat with Your PDF</div>', unsafe_allow_html=True)
 # st.markdown('<div class="content">Hello', unsafe_allow_html=True)
+
+# ========================= Upload File =========================
+SAVE_FOLDER = "file_pdf"
+os.makedirs(SAVE_FOLDER, exist_ok=True)
+
+uploaded_file = st.file_uploader("Choose a PDF file", type=["pdf"], accept_multiple_files=False)
+
+if uploaded_file is not None:
+    file_path = os.path.join(SAVE_FOLDER, uploaded_file.name)
+    with open(file_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+
+    st.session_state["file_path"] = file_path  # Simpan ke session_state
+    st.success(f"File saved to: {file_path}")
+
+# ========================= Upload File =========================
+
+# ========================= Read File =========================
+if "file_path" in st.session_state:
+    file_path = st.session_state["file_path"]
+    # st.write("File path:", file_path)
+
+    file_size = os.path.getsize(file_path)
+    # st.write(f"File size: {file_size} bytes")
+
+    # Load PDF
+    loader = PyMuPDFLoader(file_path)
+    docs = loader.load()
+
+    st.write(f"Number of pages: {len(docs)}")
+
+    def format_docs(docs):
+        return "\n\n".join([x.page_content for x in docs])
+
+    context = format_docs(docs)
+    # st.write(f"Number of pages: {context}")
+    # st.write(docs[0].page_content)
+
+# ========================= Read File =========================
 
 base_url = "http://localhost:11434"
 model = 'llama3.2:3b'
 
 # user_id = st.text_input("Enter your user id", "laxmikant")
-user_id = "laxmikant"
+user_id = "chat_pdf"
 
 def get_session_history(session_id):
     return SQLChatMessageHistory(session_id, "sqlite:///chat_history.db")
@@ -88,8 +129,19 @@ for message in st.session_state.chat_history:
 ### LLM Setup
 llm = ChatOllama(base_url=base_url, model=model)
 
-system = SystemMessagePromptTemplate.from_template("You are helpful assistant.")
-human = HumanMessagePromptTemplate.from_template("{input}")
+system = SystemMessagePromptTemplate.from_template("""You are helpful AI assistant who answer user question based on the provided context. 
+                                                    Do not answer in more than {words} words""")
+
+human_prompt = """Answer user question based on the provided context ONLY! If you do not know the answer, just say "I don't know".
+            ### Context:
+            {context}
+
+            ### Question:
+            {input}
+
+            ### Answer:"""
+
+human = HumanMessagePromptTemplate.from_template(human_prompt)
 
 messages = [system, MessagesPlaceholder(variable_name='history'), human]
 
@@ -103,7 +155,7 @@ runnable_with_history = RunnableWithMessageHistory(chain, get_session_history,
                                                    history_messages_key='history')
 
 def chat_with_llm(session_id, input):
-    for output in runnable_with_history.stream({'input': input}, config={'configurable': {'session_id': session_id}}):
+    for output in runnable_with_history.stream({'context': context, 'input': input, 'words': 50}, config={'configurable': {'session_id': session_id}}):
 
         yield output
 
